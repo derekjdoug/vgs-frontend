9fa2708cde2a5b747bde2c75dccbf9527fd790cc
Derek Douglas <derek.j.douglas13@gmail.com>
2022-08-28 22:08:56 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+import React from 'react';
+import Header from './components/header.js';
+import Contact from './components/contact.js';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Container } from 'react-bootstrap';
+
+class App extends React.Component {
+  render() {
+    return (
+      <Container>
+        <Router>
+              <Header />
+              <Routes>
+                <Route path="/contact" element={<Contact />}/>
+              </Routes>
+        </Router>
+      </Container>
+
+    )
+  }
+}
+
+export default App;
